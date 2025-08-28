4b9b3f1b054de967211c7549ca499dcd090c0e98
Caio Flôres <caio.florees2005@gmail.com>
2025-08-28 19:51:27 -0300

    fix: merge conflicts

--- src/types/Router.ts
+++ src/types/Router.ts
@@ -7,11 +7,8 @@ export const RoutesUrl = {
   TCC_DETAILS: '/tcc-details',
   NEW_TCC: '/profile/tcc/new',
   MEETINGS: '/meetings',
-<<<<<<< HEAD
   USER_DETAILS: '/user-details',
-=======
   ERROR: '/error',
->>>>>>> c49d1b6ed85390c53cb7a6c66a2c12880f2130ef
 } as const ;
 
 export type RouteUrl = typeof RoutesUrl[keyof typeof RoutesUrl];
