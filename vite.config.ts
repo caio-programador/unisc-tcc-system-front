f29281028d53bdd6e5da7d61237fff60f4626af8
João Felipe Staub <felipestaub57@gmail.com>
2025-09-18 21:16:17 -0300

    fix:build settings (#19)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ["*"],
+    allowedHosts: [".amazonaws.com", "localhost"],
   },
   plugins: [react(), tsconfigPaths()],
 });
