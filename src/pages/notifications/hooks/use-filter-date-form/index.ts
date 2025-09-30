d52df5dec84a719e2480c5729cd66d1d7d2dc42e
Caio Flôres <caio.florees2005@gmail.com>
2025-09-29 22:38:33 -0300

    fix(bypass): fixing start date filter at notification

--- src/pages/notifications/hooks/use-filter-date-form/index.ts
+++ src/pages/notifications/hooks/use-filter-date-form/index.ts
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 const SEVEN_DAYS_IN_MS = 7 * 24 * 60 * 60 * 1000;
 const currentDate = new Date();
+currentDate.setHours(0, 0, 0, 0);
 export const useFilterDateForm = () => {
   return useForm<FilterDateFormData>({
     resolver: zodResolver(filterDateFormSchema),
